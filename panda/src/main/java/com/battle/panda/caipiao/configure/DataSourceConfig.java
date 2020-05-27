316b4b2fd123364878cf2a708fe1845dc57b0f60
huxiaokang <xiaokang_code@163.com>
2020-05-28 01:58:54 +0800

    初次提交2

--- panda/src/main/java/com/battle/panda/caipiao/configure/DataSourceConfig.java
+++ panda/src/main/java/com/battle/panda/caipiao/configure/DataSourceConfig.java
@@ -1,39 +1,39 @@
-//package com.battle.panda.caipiao.configure;
-//
-//import org.apache.ibatis.session.SqlSessionFactory;
-//import org.mybatis.spring.SqlSessionFactoryBean;
-//import org.mybatis.spring.annotation.MapperScan;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.env.Environment;
-//import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-//import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//@MapperScan("com.battle.panda.caipiao.dao")
-//public class DataSourceConfig {
-//
-//    @Autowired
-//    private Environment ev;
-//
-//    @Autowired
-//    private DataSource dataSource;
-//
-//    @Bean
-//    public DataSourceTransactionManager transactionManager() {
-//        return new DataSourceTransactionManager(dataSource);
-//    }
-//
-//    @Bean
-//    public SqlSessionFactory sessionFactory() throws Exception {
-//        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-//        factoryBean.setDataSource(dataSource);
-//        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-//        factoryBean.setMapperLocations(resolver.getResources(ev.getProperty("spring.mybatis.mapperLocations")));
-//        return factoryBean.getObject();
-//    }
-//
-//}
+package com.battle.panda.caipiao.configure;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.battle.panda.caipiao.dao")
+public class DataSourceConfig {
+
+    @Autowired
+    private Environment ev;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public SqlSessionFactory sessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        factoryBean.setMapperLocations(resolver.getResources(ev.getProperty("spring.mybatis.mapperLocations")));
+        return factoryBean.getObject();
+    }
+
+}
